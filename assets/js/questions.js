2cc5c4943c7eafaf4b58051a734ab2c436316eb4
Chris Eld <chris.eld95@gmail.com>
2020-02-06 17:28:06 -0500

    added more questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,6 +2,12 @@
  
  var questionsArr = [
 {
+    question: "An element ID is denoted by a __",
+    answer: "# (hashtag)",
+    choices: [". (period)","# (hashtag)","* (asterisk)","- (hyphen)"],
+},
+
+{
     question: "What characters denote an object?",
     answer: "{ }",
     choices: ["[ ]","{ }","( )"," ~ ~"],
@@ -17,5 +23,11 @@
     question: "A boolean is a __",
     answer: "true/false value",
     choices: ["type of object","ice cream flavor","true/false value","variable"],
+},
+
+{
+    question: "An element tag is denoted by a __",
+    answer: ". (period)",
+    choices: ["# (hashtag)","- (hyphen)","* (asterisk)",". (period)"],
 }
 ]
